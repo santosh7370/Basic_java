1cfd85051e4bdeedb5c204b325aef32ea2c71abc
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2024-01-16 00:19:55 +0530

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,14 @@
+// Escape method in program with example
+
+public class Escape 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hellow \tWorld");
+        System.out.println("Hello\bWorld");
+        System.out.println("Hellow \"World\" ");
+        System.out.println("Hello \'World\' ");
+        System.out.println("Hello \\World\\ ");
+        System.out.println("Hello \nWorld");
+    }
+}
